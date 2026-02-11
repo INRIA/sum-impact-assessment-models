6da43b57df8c0bb22a532f3d0490c27da2b9b9bf
Bárbara Rodrigues <barbaracrodrigues8@gmail.com>
2026-02-11 17:27:57 +0100

    Add coefficient 'times_implemented' to distinguish groups of measures

--- src/sum_impact_assessment/schemas/core/measure.py
+++ src/sum_impact_assessment/schemas/core/measure.py
@@ -5,6 +5,8 @@ class Measure(BaseModel):
     Measure schema:
     - id: unique identifier
     - name: name of the push or pull measure
+    - times_implemented: number of times measure is implemented in living lab. Always 0 or 1 for simple measures, and 0,...,N to a group of N measures.
     """
     id: str
     name: str
+    times_implemented: int
